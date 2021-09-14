76c799ca364809f2c91bf9d5a8473ec46e3f3212
Franz Lesly Rocha <franzlesly.rocha.20@usjr.edu.ph>
2021-09-14 09:18:59 +0800

    Added Activity Files

---
+++ Assignment 1/#1/script.js
@@ -0,0 +1,29 @@
+function getArray(inputNum){
+    return inputNum;
+}
+
+
+var inputNum = prompt("Input how many numbers:");
+
+
+for(var i= 1; i<=inputNum; i++)
+    {
+        var numbers = prompt("Enter number:")
+    }
+
+        
+    if(numbers.includes("5"))
+    {
+        alert("!!!!!",  toString(inputNum));
+    }
+    else
+    {
+        alert("there's no 5 in this array");
+    }
+
+
+
+
+
+
+ 
